aa3cb3e4fc7fd8594bef2157496e18fcebe2d9ca
Gaurav <48078706+gauravkarki93@users.noreply.github.com>
2019-08-09 13:41:59 -0700

    Update Valid Parenthesis.java

--- Valid Parenthesis.java
+++ Valid Parenthesis.java
@@ -1,5 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length() == 0)
+            return false;
+        Stack<Character> stack = new Stack<Character>();
+        stack.push(s.charAt(0));
+        int i = 0;
+        while(i<s.length())
+        {            
+            char currChar = s.charAt(i);
+            if(currChar == '(' ||currChar == '[' || currChar == '{')
+            {
+                stack.push(currChar);
+            }
+            else if(stack.empty())
+            {
+                return false;
+            }
+            else //Pop when you find closing brackets and if it doesn't matches to curr Char then return false
+            {
+                char poppedChar = stack.pop();
+                if((poppedChar == '{' && currChar != '}' ) || (poppedChar == '[' && currChar != ']' ) || (poppedChar == '(' && currChar != ')' ))
+                    return false;
+            }
+            i++;
+        }
+        if(!stack.empty())
+            return false;
         
+        return true;
     }
 }
